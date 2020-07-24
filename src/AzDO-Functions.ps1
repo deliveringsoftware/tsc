13b62e1f39d7969843b4b262b026a871a966a6b9
Emmanuel G. Brandão <egomesbrandao@gmail.com>
2020-07-23 21:44:01 -0300

    Add the logic to add new users (#14)
    * Moved other users manipulation fn to AzDO fn file
    * Added the logic of check and add new users
    
    Co-authored-by: = <=>

--- src/AzDO-Functions.ps1
+++ src/AzDO-Functions.ps1
@@ -14,7 +14,7 @@ function Add-UsersToGroup {
     Write-Host "=== Inserting users on $groupDisplayName group ==="
 
     foreach ($user in $users) {
-        Write-Host ("=== Inserting user {0} ===" -f $user.name)
+        Write-Host ("=== Inserting user {0} to group '$($ymlGroup.name)' ===" -f $user.name)
         az devops security group membership add --group-id $groupDescriptor --member-id $user.email --org $Organization | Out-Null
     }
 }
@@ -37,5 +37,43 @@ function Remove-UsersToGroup {
     foreach ($user in $users) {
         Write-Host ("=== Removing user {0} ===" -f $user.name)
         az devops security group membership remove --group-id $groupDescriptor --member-id $user.email --org $Organization -y | Out-Null
+    }
+}
+
+function Get-MembersFromGroup {
+    param (
+        [string]$organizationUrl,
+        [string]$teamProjectName,
+        [string]$groupName,
+        [ValidateSet("user","group")]
+        [string]$kind = "user"
+    )
+
+    $userGroup = Get-UserGroup -Organization $organizationUrl -teamProject $teamProjectName -group $groupName
+
+    $groupMembers = $(az devops security group membership list --id $userGroup.Descriptor --relationship members --query "*.{name: displayName, principalName: principalName, kind: subjectKind}" -o json) | ConvertFrom-Json
+
+    return $groupMembers | Where { $_.kind -eq $kind }
+}
+
+function Get-UserGroup {
+    param (
+        [string]$organizationUrl,        
+        [string]$teamProjectName,
+        [string]$groupName
+    )
+
+    $groupDescriptor = $(az devops security group list --organization $organizationUrl `
+                            --project $teamProjectName `
+                            --query "graphGroups[?contains(displayName, '$groupName')].descriptor" -o tsv)
+    $groupDisplayName = $(az devops security group list --organization $organizationUrl --project $teamProjectName --query "graphGroups[?contains(displayName, '$group')].displayName" -o tsv)
+
+    if(-not ($groupDescriptor)){
+        Write-Error "User Group no found"
+    }
+
+    return New-Object PSObject -Property @{
+        Descriptor       = $groupDescriptor
+        DisplayName     = $groupDisplayName
     }
 }
